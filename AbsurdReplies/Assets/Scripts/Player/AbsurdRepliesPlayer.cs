45e0adef031aa285e620c716cd41743404dcef02
albpb2 <albpb2@gmail.com>
2020-12-08 00:12:06 +0000

    Make player notify when they're connected

---
+++ AbsurdReplies/Assets/Scripts/Player/AbsurdRepliesPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Mirror;
+
+namespace AbsurdReplies.Player
+{
+    public class AbsurdRepliesPlayer : NetworkBehaviour
+    {
+        public delegate void PlayerConnectedDelegate(NetworkIdentity networkIdentity);
+        public static event PlayerConnectedDelegate onPlayerConnected;
+        
+        private async void Start()
+        {
+            NotifyConnected(GetComponent<NetworkIdentity>());
+        }
+
+        [Command]
+        public void NotifyConnected(NetworkIdentity networkIdentity)
+        {
+            onPlayerConnected?.Invoke(networkIdentity);
+        }
+    }
+}
---
+++ AbsurdReplies/Assets/Scripts/Server/LobbyPlayerListUpdater.cs
@@ -0,0 +1,34 @@
+﻿using AbsurdReplies.Player;
+using Mirror;
+using UnityEngine;
+
+namespace AbsurdReplies.Server
+{
+    public class LobbyPlayerListUpdater : NetworkBehaviour
+    {
+        private void Awake()
+        {
+            if (!isServer)
+            {
+                Debug.Log($"Not server, destroying {nameof(LobbyPlayerListUpdater)}");
+                Destroy(gameObject);
+                return;
+            }
+        }
+
+        private void OnEnable()
+        {
+            AbsurdRepliesPlayer.onPlayerConnected += HandlePlayerConnected;
+        }
+
+        private void OnDisable()
+        {
+            AbsurdRepliesPlayer.onPlayerConnected += HandlePlayerConnected;
+        }
+
+        private void HandlePlayerConnected(NetworkIdentity networkIdentity)
+        {
+            Debug.Log($"Adding player {networkIdentity.netId}");
+        }
+    }
+}
