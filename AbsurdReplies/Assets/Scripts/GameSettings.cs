d54612be11a5fcba472e6eaecb4b68dbd27563d1
albpb2 <albpb2@gmail.com>
2020-12-08 12:55:53 +0000

    Additional game settings

--- AbsurdReplies/Assets/Scripts/GameSettings.cs
+++ AbsurdReplies/Assets/Scripts/GameSettings.cs
@@ -5,7 +5,16 @@ namespace AbsurdReplies
     public class GameSettings : MonoSingleton<GameSettings>
     {
         private int DefaultMinPlayers = 2;
+        private int DefaultTargetScore = 15;
+        private int DefaultRoundTimeSeconds = 60;
+        private int DefaultVotingTimeSeconds = 60;
 
         public int MinPlayers => DefaultMinPlayers;
+
+        public int TargetScore => DefaultTargetScore;
+
+        public int RoundTimeSeconds => DefaultRoundTimeSeconds;
+
+        public int VotingTimeSeconds => 60;
     }
 }
