e4b4462200c7c21fbe664165588fcf6eee241857
albpb2 <albpb2@gmail.com>
2020-12-08 13:40:39 +0000

    Shuffle players on game start

---
+++ AbsurdReplies/Assets/Scripts/AbsurdRepliesGame.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Mirror;
+
+namespace AbsurdReplies
+{
+    public class AbsurdRepliesGame : NetworkBehaviour
+    {
+        [SyncVar] private int _currentPlayerIndex;
+
+        private Random _random;
+        
+        // Only in server
+        private List<NetworkConnectionToClient> _playerConnections;
+
+        private async void Awake()
+        {
+            _random = new Random();
+        }
+
+        private async void Start()
+        {
+            if (isServer)
+            {
+                _playerConnections = NetworkServer.connections.Values.ToList();
+                _playerConnections.Shuffle(_random);
+            }
+        }
+    }
+}
