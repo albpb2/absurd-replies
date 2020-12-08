0b62ca3f38e17a03d794186c5f0d60872a91b402
albpb2 <albpb2@gmail.com>
2020-12-08 19:34:18 +0000

    Create question categories

---
+++ AbsurdReplies/Assets/Scripts/QuestionCategories.cs
@@ -0,0 +1,12 @@
+﻿namespace AbsurdReplies
+{
+    public enum QuestionCategory
+    {
+        Unknown = 0,
+        Words = 1,
+        People = 2,
+        Initials = 3,
+        Movies = 4,
+        Dates = 5
+    }
+}
