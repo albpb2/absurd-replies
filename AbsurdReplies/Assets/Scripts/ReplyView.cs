03d8b9cfa4e1f7275c6c4e7dea875cebb27d1852
albpb2 <albpb2@gmail.com>
2020-12-21 23:41:37 +0100

    Send replies to server

---
+++ AbsurdReplies/Assets/Scripts/ReplyView.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace AbsurdReplies
+{
+    public class ReplyView : MonoBehaviour
+    {
+        [SerializeField] private RoundReplies _roundReplies;
+        [SerializeField] private TMP_InputField _replyInputField;
+
+        private void Awake()
+        {
+            DependencyValidator.ValidateDependency(_roundReplies, nameof(_roundReplies), nameof(ReplyView));
+            DependencyValidator.ValidateDependency(_replyInputField, nameof(_replyInputField), nameof(ReplyView));
+        }
+
+        public async void SendReply()
+        {
+            if (string.IsNullOrEmpty(_replyInputField.text))
+            {
+                return;
+            }
+            
+            _roundReplies.SendReply(_replyInputField.text);
+        }
+    }
+}
---
+++ AbsurdReplies/Assets/Scripts/RoundReplies.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+namespace AbsurdReplies
+{
+    public class RoundReplies : NetworkBehaviour
+    {
+        private Dictionary<int, string> _replies;
+
+        private void Awake()
+        {
+            _replies = new Dictionary<int, string>();
+        }
+
+        [Command(ignoreAuthority = true)]
+        public async void SendReply(string reply, NetworkConnectionToClient sender = null)
+        {
+            Debug.Log($"Received reply from {sender.connectionId}: {reply}");
+            _replies[sender.connectionId] = reply;
+        }
+    }
+}
