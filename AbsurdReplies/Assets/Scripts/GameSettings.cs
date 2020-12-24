9138759e3e151bfc7139149d538d9642657bd41f
albpb2 <albpb2@gmail.com>
2020-12-24 19:05:47 +0100

    Add settings for points

--- AbsurdReplies/Assets/Scripts/GameSettings.cs
+++ AbsurdReplies/Assets/Scripts/GameSettings.cs
@@ -9,11 +9,17 @@ namespace AbsurdReplies
         private const int DefaultTargetScore = 15;
         private const int DefaultRoundTimeSeconds = 10;
         private const int DefaultVotingTimeSeconds = 60;
+        private const int DefaultPointsPerCorrectGuess = 2;
+        private const int DefaultPointsPerVoteReceived = 1;
+        private const int DefaultPointForNotGuessedAnswer = 3;
 
         [SerializeField] private int _minPlayers = DefaultMinPlayers;
         [SerializeField] private int _targetScore = DefaultTargetScore;
         [SerializeField] private int _roundTimeSeconds = DefaultRoundTimeSeconds;
         [SerializeField] private int _votingTimeSeconds = DefaultVotingTimeSeconds;
+        [SerializeField] private int _pointsPerCorrectGuess = DefaultPointsPerCorrectGuess;
+        [SerializeField] private int _pointsPerVoteReceived = DefaultPointsPerVoteReceived;
+        [SerializeField] private int _pointsForNotGuessedAnswer = DefaultPointForNotGuessedAnswer;
 
         public int MinPlayers => _minPlayers;
 
@@ -22,5 +28,11 @@ namespace AbsurdReplies
         public int RoundTimeSeconds => _roundTimeSeconds;
 
         public int VotingTimeSeconds => _votingTimeSeconds;
+
+        public int PointsPerCorrectGuess => _pointsPerCorrectGuess;
+
+        public int PointsPerVoteReceived => _pointsPerVoteReceived;
+
+        public int PointsForNotGuessedAnswer => _pointsForNotGuessedAnswer;
     }
 }
